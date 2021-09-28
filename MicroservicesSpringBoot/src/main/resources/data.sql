462d230b0815984252b8e326009fd614d97f6e42
Iskander Dosgali <idosgali@gmail.com>
2021-09-28 15:14:09 +0600

    Проверка

---
+++ MicroservicesSpringBoot/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','KZT',65,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','KZT',75,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'AUD','KZT',25,0);
