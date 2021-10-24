a8c1541d00b1207aff917af4f15155d23bdda487
Iskander Dosgali <idosgali@gmail.com>
2021-10-24 21:58:24 +0600

    Добавил Lombook

--- MicroservicesSpringBoot/src/main/java/com/example/forex/ExchangeValue.java
+++ MicroservicesSpringBoot/src/main/java/com/example/forex/ExchangeValue.java
@@ -1,12 +1,15 @@
 package com.example.forex;
 
 import java.math.BigDecimal;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class ExchangeValue {
 
     @Id
@@ -34,28 +37,28 @@ public class ExchangeValue {
         this.conversionMultiple = conversionMultiple;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public BigDecimal getConversionMultiple() {
-        return conversionMultiple;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public String getFrom() {
+//        return from;
+//    }
+//
+//    public String getTo() {
+//        return to;
+//    }
+//
+//    public BigDecimal getConversionMultiple() {
+//        return conversionMultiple;
+//    }
+//
+//    public int getPort() {
+//        return port;
+//    }
+//
+//    public void setPort(int port) {
+//        this.port = port;
+//    }
 
 }
