ad2fbfaa384a0ecdac713c21c4b36e91ae327d28
Iskander Dosgali <idosgali@gmail.com>
2021-10-26 22:55:48 +0600

    Добавил Hystrix and Dashboard

--- MicroservicesSpringBoot/src/main/java/com/example/forex/ForexApplication.java
+++ MicroservicesSpringBoot/src/main/java/com/example/forex/ForexApplication.java
@@ -4,9 +4,14 @@ package com.example.forex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@Controller
+@EnableHystrixDashboard
 public class ForexApplication {
 
     public static void main(String[] args) {
