b97847ff2f249c5b3b94a8e22cc3dab406546754
Iskander Dosgali <idosgali@gmail.com>
2021-11-12 16:20:12 +0600

    add zuul service

---
+++ MicroservicesSpringBoot6/src/test/java/com/example/grateway/GratewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
